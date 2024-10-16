6349b808c85f762e2bc2d8a1a3dc384b0d12e44e
IE Dev 2 <ie.dev.2@hotmail.com>
2024-10-16 10:10:06 -0700

    73. Tarea filtrar por UUID

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -9,15 +9,12 @@ import { Character } from '../../interfaces/character.interface';
 export class ListComponent {
 
   @Input()
-  public characterList: Character[] = [{
-    name: 'Trunks',
-    power: 10
-  }];
+  public characterList: Character[] = [];
 
   @Output()
-  public onDelete: EventEmitter<number> = new EventEmitter();
+  public onDelete: EventEmitter<string> = new EventEmitter();
 
-  onDeleteCharacter(index:number): void {
-    this.onDelete.emit(index);
+  onDeleteCharacter(id:string): void {
+    this.onDelete.emit(id);
   }
 }
